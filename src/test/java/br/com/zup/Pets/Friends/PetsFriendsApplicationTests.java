2029f85a7f59ec65688d34bee2bc980c909225e1
natalia.palota <natalia.palota@zup.com.br>
2021-03-15 13:37:58 -0300

    Criar projeto Pets Friends

---
+++ src/test/java/br/com/zup/Pets/Friends/PetsFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Pets.Friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
